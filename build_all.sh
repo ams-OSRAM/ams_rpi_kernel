bcfab7f90d64a1e6fbf8c0b409bf92c6a1c772e0
Zhenyu Ye <zhenyu.ye@ams-osram.com>
2022-04-07 17:46:21 +0200

    Small fix on the master build script.

--- build_all.sh
+++ build_all.sh
@@ -4,14 +4,14 @@ TAG=rpi-5.15.y
 git clone --depth 1 --branch $TAG https://github.com/raspberrypi/linux.git
 
 # apply patches and sources
-cd mira220/patch-$TAG && ./apply_patch.sh
-cd mira220/src && ./apply_src.sh
+(cd mira220/patch-$TAG && ./apply_patch.sh)
+(cd mira220/src && ./apply_src.sh)
 
 # prepare compilation script
 ln -s $PWD/common/build.sh ./linux/build.sh
 
 # config, build, and install kernel
-cd ./linux && ./build.sh restoreconfig
-cd ./linux && ./build.sh build
-cd ./linux && ./build.sh install
+(cd ./linux && ./build.sh restoreconfig)
+(cd ./linux && ./build.sh build)
+(cd ./linux && ./build.sh install)
 
