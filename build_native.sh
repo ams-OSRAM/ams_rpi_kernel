9a81992000fd4e53094f1379d9d21d818d09668a
zye <zhenyu.ye@ams-osram.com>
2022-09-22 11:47:01 +0200

    Put dtb inside deb pkg as well. Backup dtbo before install deb pkg.

--- build_native.sh
+++ build_native.sh
@@ -23,25 +23,9 @@ then
 	fi
 fi
 
-
-echo "#################"
-echo "# Device tree"
-echo "#################"
-
-for dtname in "mira220" "mira220color" "mira050" "mira050color"; do
-	dtdir=$(echo $dtname | sed 's/color//')
-	echo "Building device tree source $dtdir/src/$dtname.dts"
-	(cd $dtdir/src && cpp -nostdinc -I include -I arch -I/usr/src/linux-headers-${KERNELRELEASE}/include/ -undef -x assembler-with-cpp  $dtname-overlay.dts $dtname-overlay.dts.preprocessed)
-	(cd $dtdir/src && dtc -I dts -O dtb $dtname-overlay.dts.preprocessed -o $dtname.dtbo)
-	(cd $dtdir/src && rm $dtname-overlay.dts.preprocessed)
-	echo "Copying device tree blob overlay $dtdir/src/$dtname.dtbo to /boot/overlays/"
-	sudo cp $dtdir/src/$dtname.dtbo /boot/overlays/
-done
-
-echo "#################"
-echo "# Driver module"
-echo "#################"
-
+echo "#############################"
+echo "# Creating deb pkg folder"
+echo "#############################"
 
 # module name should not have underscore
 PKGNAME="mira-driver"
@@ -63,6 +47,26 @@ Maintainer: Zhenyu Ye <zhenyu.ye@ams-osram.com>
 Description: Mira driver for RPI.
  It contains mira220, mira220color, mira050, mira050color." > $PKGDIR/DEBIAN/control
 
+echo "#############################"
+echo "# Add device tree to deb"
+echo "#############################"
+
+mkdir -p $PKGDIR/boot/overlays
+
+for dtname in "mira220" "mira220color" "mira050" "mira050color"; do
+	dtdir=$(echo $dtname | sed 's/color//')
+	echo "Building device tree source $dtdir/src/$dtname.dts"
+	(cd $dtdir/src && cpp -nostdinc -I include -I arch -I/usr/src/linux-headers-${KERNELRELEASE}/include/ -undef -x assembler-with-cpp  $dtname-overlay.dts $dtname-overlay.dts.preprocessed)
+	(cd $dtdir/src && dtc -I dts -O dtb $dtname-overlay.dts.preprocessed -o $dtname.dtbo)
+	(cd $dtdir/src && rm $dtname-overlay.dts.preprocessed)
+	echo "Copying device tree blob overlay $dtdir/src/$dtname.dtbo to $PKGDIR/boot/overlays/"
+	sudo cp $dtdir/src/$dtname.dtbo $PKGDIR/boot/overlays/
+done
+
+echo "#############################"
+echo "# Add driver module to deb"
+echo "#############################"
+
 MODULEDIR=$PKGDIR/usr/lib/modules/$KERNELRELEASE/kernel/drivers/media/i2c
 mkdir -p $MODULEDIR
 
@@ -74,8 +78,18 @@ mkdir -p $MODULEDIR
 (cd mira050/src && make INSTALL_MOD_PATH=$PKGDIR install)
 (cd mira050/src && make clean)
 
+echo "#############################"
+echo "# Build and install deb pkg"
+echo "#############################"
+
 echo "Building $PKGDIR.deb"
 dpkg-deb --build --root-owner-group $PKGDIR
+
+echo "Backing up mira dtbo in /boot/overlays"
+for file in `ls /boot/overlays/mira*.dtbo`; do
+	echo "Moving ${file} to ${file}bak"
+	sudo mv ${file} ${file}bak
+done
 
 echo "Installing $PKGDIR.deb"
 sudo dpkg -i $PKGDIR.deb
