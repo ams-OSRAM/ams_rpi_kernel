19bd774d826e161d1159e53bef780160270cf7a3
pbae <philippe.baetens@ams-osram.com>
2024-10-21 09:22:34 +0200

    mira016 related topics. mira016 fix 10b fine gain

--- mira016/src/apply_src.sh
+++ mira016/src/apply_src.sh
@@ -3,4 +3,5 @@ PATCH_PATH=.
 cp $PATCH_PATH/mira016_mono_color-overlay.dtsi $LINUX_PATH/arch/arm/boot/dts/overlays/
 cp $PATCH_PATH/mira016-overlay.dts $LINUX_PATH/arch/arm/boot/dts/overlays/
 cp $PATCH_PATH/mira016.inl $LINUX_PATH/drivers/media/i2c/
+cp $PATCH_PATH/mira016_registers.inl $LINUX_PATH/drivers/media/i2c/
 cp $PATCH_PATH/mira016.c $LINUX_PATH/drivers/media/i2c/
